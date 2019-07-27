2aaca958ed1a9b729d67a2b0087d7e703c18c315
unknown <976912502@qq.com>
2019-07-27 12:33:48 +0800

    ui

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/signin')
+def signin_page():
+    return render_template('signin.html')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    if request.form['username'] == "admin" and request.form['password'] == "123456":
+        return redirect("/")
+    else:
+        return render_template('signin.html')
+
+
+@app.route('/list')
+def _list():
+    return render_template('list.html')
+
+
+@app.route('/setting')
+def setting():
+    return render_template('setting.html')
 
 if __name__ == '__main__':
     app.run()
